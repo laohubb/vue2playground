3765237c74131ea79445fbc2389818c47b309699
laohu <laohubbb@gmail.com>
2023-06-14 23:07:11 +0800

    边缘滑动监听

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,56 @@
 <template>
-  <div class="home">
-    <i class="el-icon-edit"></i>
-    <i class="el-icon-share"></i>
-    <i class="el-icon-delete"></i>
-    <el-button type="primary" icon="el-icon-search">搜索</el-button>
+  <div>
+    <div class="header" ref="headerRef">header</div>
+    <div class="home" ref="homeRef">
+      <!-- 其他内容 -->
+      <div style="height: 500px"></div>
+      <input @focus="handleInputFocus" @blur="handleInputBlur" />
+      {{ headerTop }}
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "HomeView",
-  components: {},
+  data() {
+    return {
+      isKeyboardVisible: false,
+      headerTop: 0,
+    };
+  },
+  mounted() {},
+  methods: {
+    getHeaderTop() {
+      const headerElement = this.$refs.headerRef;
+      const headerRect = headerElement.getBoundingClientRect();
+      const headerTop = headerRect.top;
+      this.headerTop = headerTop;
+    },
+    resizeChange() {},
+    handleInputFocus() {
+      this.isKeyboardVisible = true;
+      const headerElement = this.$refs.headerRef;
+      setTimeout(() => {
+        this.getHeaderTop();
+        headerElement.style.top = -this.headerTop + 20 + "px";
+      }, 100);
+    },
+    handleInputBlur() {
+      this.isKeyboardVisible = false;
+      this.$refs.headerRef.style.top = 20 + "px";
+    },
+  },
 };
 </script>
+
+<style lang="scss">
+.header {
+  position: fixed;
+  //top: 100px;
+  width: 100%;
+  height: 50px;
+
+  background-color: #f3aeae;
+}
+</style>

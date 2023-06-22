6a22de20ab646b602843eced8ca505986a135e33
laohu <laohubbb@gmail.com>
2023-06-22 12:45:17 +0800

    update

--- src/main.js
+++ src/main.js
@@ -6,7 +6,14 @@ import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import Vant from "vant";
 import "vant/lib/index.css";
+import VConsole from "vconsole";
 
+// 实例化 VConsole
+const vconsole = new VConsole();
+// 可选：在开发环境下自动显示 VConsole
+if (process.env.NODE_ENV === "development") {
+  vconsole.show();
+}
 Vue.use(Vant);
 Vue.use(ElementUI);
 Vue.config.productionTip = false;

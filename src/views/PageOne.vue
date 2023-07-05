1f2fd6e1d5141fc75946cd20ebf03fad8816831f
laohu <laohubbb@gmail.com>
2023-07-05 23:25:49 +0800

    update

--- src/views/PageOne.vue
+++ src/views/PageOne.vue
@@ -1,15 +1,23 @@
 <template>
-  <div>
+  <div style="margin-top: 10px">
     <van-collapse v-model="activeNames">
-      <van-collapse-item title="标题1" name="1" icon="chat-o">
+      <van-collapse-item name="1" :is-link="false">
+        <template #title>
+          <div>
+            <i class="van-icon van-icon-arrow van-cell__right-icon" />系统分组
+          </div>
+        </template>
         <van-collapse v-model="activeNames2">
-          <van-collapse-item title="标题1" name="7">内容</van-collapse-item>
-          <van-collapse-item title="标题2" name="8">内容</van-collapse-item>
-          <van-collapse-item title="标题3" name="9">内容</van-collapse-item>
+          <van-collapse-item title="标题1" name="7" :is-link="false">
+            <template #title>
+              <div>
+                <i class="van-icon van-icon-arrow van-cell__right-icon" />标签1
+              </div>
+            </template>
+            内容
+          </van-collapse-item>
         </van-collapse>
       </van-collapse-item>
-      <van-collapse-item title="标题2" name="2">内容</van-collapse-item>
-      <van-collapse-item title="标题3" name="3">内容</van-collapse-item>
     </van-collapse>
   </div>
 </template>
@@ -26,4 +34,11 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="less" scoped>
+[class*="van-hairline"]::after {
+  border: unset !important;
+}
+::v-deep .van-collapse-item__title--expanded::after {
+  border: unset !important;
+}
+</style>
--- src/views/PageSix.vue
+++ src/views/PageSix.vue
@@ -1,59 +1,30 @@
 <template>
   <div>
     <input type="file" @change="handleImageChange" />
-    <canvas ref="canvas"></canvas>
-    <button @click="compressImage">压缩图片</button>
+    <img :src="imgUrl" id="myImages" class="img" />
   </div>
 </template>
 
 <script>
+import GIF from "gif.js";
+
 export default {
   data() {
     return {
       scale: 1,
-      selectedImage: null,
+      imgUrl: null,
       canvasWidth: 800,
       canvasHeight: 600,
     };
   },
   methods: {
     handleImageChange(event) {
-      this.selectedImage = event.target.files[0];
-    },
-    compressImage() {
-      const canvas = this.$refs.canvas;
-      const context = canvas.getContext("2d");
-      const reader = new FileReader();
-
-      reader.onload = (event) => {
-        const img = new Image();
-        img.onload = () => {
-          let { width, height } = img;
-          const ratio = Math.min(
-            this.canvasWidth / width,
-            this.canvasHeight / height
-          );
-
-          width *= ratio;
-          height *= ratio;
-
-          canvas.width = width;
-          canvas.height = height;
-
-          context.clearRect(0, 0, width, height);
-          context.drawImage(img, 0, 0, width, height);
-
-          const compressedImageData = canvas.toDataURL("image/jpeg", 0.2);
-
-          // 在此处将压缩后的图像数据上传到服务器或其他操作
-          console.log(compressedImageData);
-        };
-
-        img.src = event.target.result;
-      };
-
-      reader.readAsDataURL(this.selectedImage);
+      this.imgUrl = URL.createObjectURL(event.target.files[0]);
     },
   },
 };
 </script>
+<style>
+.img {
+}
+</style>
